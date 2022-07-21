9c5bbc863634d70bc1e79173b1357343bdef8932
maxicasella <maxicasella@gmail.com>
2022-07-20 23:47:02 -0300

    Enemigos

---
+++ Assets/Assets/Scripts/UI/Movement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Transform enemie;
+
+    private void FixedUpdate()
+    {
+
+        var pos = transform.position;
+        pos = enemie.position;
+    }
+}
