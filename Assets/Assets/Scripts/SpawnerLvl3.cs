1e232cd2b6b24688000aa248b46adea8c817ae00
maxicasella <maxicasella@gmail.com>
2022-07-28 23:25:02 -0300

    Prefabs

---
+++ Assets/Assets/Scripts/SpawnerLvl3.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerLvl3 : MonoBehaviour
+{
+    [SerializeField] EnemiesDeath _kills;
+    [SerializeField] GameObject _enemieSword;
+    [SerializeField] GameObject _enemieSpear;
+    [SerializeField] Transform _spawnA;
+    [SerializeField] Transform _spawnB;
+
+    public float _spawn = 1;
+
+    private void Update()
+    {
+        if (_spawn == 1)
+        {
+            switch (_kills.killsCounter)
+            {
+                case 2:
+                    Instantiate(_enemieSword, _spawnB.position, Quaternion.identity);
+                    _spawn = 0;
+                    break;
+                case 4:
+                    Instantiate(_enemieSpear, _spawnA.position, Quaternion.identity);
+                    _spawn = 0;
+                    break;
+                case 6:
+                    Instantiate(_enemieSword, _spawnB.position, Quaternion.identity);
+                    _spawn = 0;
+                    break;
+                case 8:
+                    Instantiate(_enemieSpear, _spawnA.position, Quaternion.identity);
+                    _spawn = 0;
+                    break;
+                case 10:
+                    Instantiate(_enemieSword, _spawnB.position, Quaternion.identity);
+                    _spawn = 0;
+                    break;
+                case 12:
+                    Instantiate(_enemieSpear, _spawnA.position, Quaternion.identity);
+                    _spawn = 0;
+                    break;
+
+            }
+
+        }
+
+    }
+}
+
+
