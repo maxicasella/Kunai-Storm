ebd4a72b54a9f76bd616e32020ef4cfb16475ad5
maxicasella <maxicasella@gmail.com>
2022-07-31 17:03:06 -0300

    Portal

---
+++ Assets/Assets/Scripts/Objects/Portal.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    [SerializeField] GameObject _trigger;
+    [SerializeField] GameObject _humo;
+    [SerializeField] GameObject _bloqueoCamino;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        _humo.SetActive(true);
+        Destroy(_trigger);
+        Destroy(_bloqueoCamino);
+    }
+
+
+}
