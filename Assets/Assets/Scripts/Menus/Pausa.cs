b8694cb080ba24d9a2d924aa828d5359c3ddfe0f
maxicasella <maxicasella@gmail.com>
2022-08-02 00:49:52 -0300

    Pausa

--- Assets/Assets/Scripts/Menus/Pausa.cs
+++ Assets/Assets/Scripts/Menus/Pausa.cs
@@ -1,18 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Pausa : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    bool _menu = false;
+    public GameObject menuPausa;
+    public PlayerAttack _playerC;
+
+
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (_menu)
+            {
+                Continue();
+            }
+            else
+            {
+                Pause();
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void Pause()
     {
-        
+        _playerC.enabled = false;
+        _menu = true;
+        Time.timeScale = 0f;
+        menuPausa.SetActive(true);
+    }
+
+    public void Continue()
+    {
+        _playerC.enabled = true;
+        _menu = false;
+        Time.timeScale = 1f;
+        menuPausa.SetActive(false);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
