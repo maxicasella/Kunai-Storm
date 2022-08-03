0c9c4cbe5868ad52e6fc83b0348c108a60752bdb
maxicasella <maxicasella@gmail.com>
2022-08-02 22:52:03 -0300

    Sonidos P1

--- Assets/Assets/Scripts/Objects/Key.cs
+++ Assets/Assets/Scripts/Objects/Key.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class Key : MonoBehaviour
 {
+
+    [SerializeField] AudioSource _audio;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         PlayerStats lvlkey = collision.GetComponent<PlayerStats>();
         if(lvlkey!=null)
         {
+            _audio.Play();
             lvlkey.Key();
             Destroy(this.gameObject);
         }
