62d9da1d5d52b7e120aef04493c52e3700b43ca2
maxicasella <maxicasella@gmail.com>
2022-08-04 19:55:03 -0300

    Escudo Nigromante

---
+++ Assets/Assets/Scripts/Enemies/Necromancer/AuraShield.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AuraShield : MonoBehaviour
+{
+    [SerializeField] float _shiedlHP;
+    [SerializeField] AudioSource _audioLoop;
+    [SerializeField] AudioSource _audioBreak;
+    [SerializeField] GameObject _particulas;
+
+    private void Update()
+    {
+        if (_shiedlHP <= 0)
+        {
+            _audioLoop.Stop();
+            _audioBreak.Play();
+            Instantiate(_particulas, transform.position, Quaternion.identity);
+        }
+    }
+}
