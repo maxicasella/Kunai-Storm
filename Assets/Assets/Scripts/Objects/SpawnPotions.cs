d526c5b40e79507d119e04e22c84701da9756d7a
maxicasella <maxicasella@gmail.com>
2022-08-05 01:43:44 -0300

    Spawn potions

---
+++ Assets/Assets/Scripts/Objects/SpawnPotions.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPotions : MonoBehaviour
+{
+    [SerializeField] PlayerStats _stats;
+    [SerializeField] Transform _spawnMana;
+    [SerializeField] Transform _spawnHp;
+    [SerializeField] GameObject _hpPotion;
+    [SerializeField] GameObject _manaPotion;
+
+    [SerializeField] float _spawns = 2;
+
+    private void Update()
+    {
+        if(_stats.playerHp < 100 && _spawns == 2)
+        {
+            Instantiate(_hpPotion, _spawnHp.position, Quaternion.identity);
+            _spawns--;
+        }
+
+        if (_stats.playerMana < 50 && _spawns == 1)
+        {
+            Instantiate(_manaPotion, _spawnMana.position, Quaternion.identity);
+            _spawns--;
+        }
+    }
+
+}
